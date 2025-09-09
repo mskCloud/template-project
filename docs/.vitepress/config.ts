6ac0347780795691399ee7e8940ae4618f11d27e
youmeijin <youmeijin-fesco@sunlands.com>
2025-09-09 15:43:32 +0800

    update: doc base url

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+    base: '/repo/',
     title: 'Template Project',
     description: 'More Template For Common Dev',
     themeConfig: {
